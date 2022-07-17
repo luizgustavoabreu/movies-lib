4b6ff0326f583a6c5e880c3ccd92c21d28ec93e8
Luiz Gustavo <72631018+luizgustavoabreu@users.noreply.github.com>
2022-07-17 13:22:12 -0300

    Configurando e formatando a página que exibe detalhes dos filmes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,6 @@ import './MoviesGrid.css';
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
-
 const Home = () => {
   const [topMovies, setTopMovies] = useState([]);
 
